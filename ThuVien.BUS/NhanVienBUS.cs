96caa5012c026d9c679bb8391806463b5dbb9773
Gia Phuc <phuctgps42473@gmail.com>
2024-10-01 21:37:42 +0700

    modify App and build Session Management

---
+++ ThuVien.BUS/NhanVienBUS.cs
@@ -0,0 +1,37 @@
+﻿using ThuVien.DAL;
+using ThuVien.DTO;
+
+
+namespace ThuVien.BUS
+{
+    public class NhanVienBus
+    {
+        private NhanVienDAL nhanVienDAL;
+
+        public NhanVienBus()
+        {
+            nhanVienDAL = new NhanVienDAL();
+        }
+
+        public bool ValidateLogin(string email, string password)
+        {
+            NhanVienDTO NhanVien = nhanVienDAL.GetNhanVienByEmail(email);
+
+            if (NhanVien != null)
+            {
+                if (NhanVien.MatKhau == password)
+                {
+                    Session.Instance.SetNhanVien(NhanVien);
+                    return true;
+                }
+            }
+            return false;
+
+        }
+
+        public void logout()
+        {
+            Session.Instance.clearNhanVien();
+        }
+    }
+}
---
+++ ThuVien.BUS/Session.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThuVien.DTO;
+
+namespace ThuVien.BUS
+{
+    public class Session
+    {
+        private static Session _instance;
+        public NhanVienDTO NhanVien { get; set; }
+
+        public static Session Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new Session();
+                }
+                return _instance;
+
+
+            }
+        }
+
+        public void SetNhanVien(NhanVienDTO nv)
+        {
+            this.NhanVien = nv;
+        }
+
+        public void clearNhanVien()
+        {
+            this.NhanVien = null;
+
+        }
+
+
+    }
+
+
+}
