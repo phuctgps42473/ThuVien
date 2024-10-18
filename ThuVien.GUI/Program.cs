4903d1fdf6dcf1e233f3ad79994864aa9ea8bbca
JunnX01 <linhpc1106@gmail.com>
2024-10-18 18:13:56 +0700

    fix

--- ThuVien.GUI/Program.cs
+++ ThuVien.GUI/Program.cs
@@ -16,7 +16,7 @@ namespace ThuVien.GUI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new PhieuMuonForm());
+            Application.Run(new DangNhapForm());
         }
     }
 }
